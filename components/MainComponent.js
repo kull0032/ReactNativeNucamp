a8c69a809edd07fd9c420797d9056389d93ef033
Jordan Kullmann <jordankullmann@gmail.com>
2021-06-25 14:26:02 -0500

    Using Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,15 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Icon } from 'react-native-elements';
 import  SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -199,6 +208,13 @@ const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
     
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPartners();
+        this.props.fetchPromotions();
+    }
+
     render() {
         return (
             <View style={{
@@ -240,4 +256,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps) (Main);
