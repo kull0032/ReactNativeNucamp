80888d8c766ea2770b97c048b09427bdcf6719af
Jordan Kullmann <jordankullmann@gmail.com>
2021-07-01 11:25:02 -0500

    Week 3 gestures part 2 complete

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,8 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl'
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
+import Animated from 'react-native-reanimated';
 
 const mapStateToProps = state => {
     return {
@@ -46,22 +48,26 @@ class About extends Component {
         if (this.props.partners.isLoading) {
             return (
                 <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Loading />
-                    </Card>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Loading />
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                    </Card>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 
 class Contact extends Component {
@@ -12,21 +13,13 @@ class Contact extends Component {
     render() {
         return(
             <ScrollView>
-                <Card title = "Contact Information"
-                    wrapperStyle={{margin: 20}} >
-                    {
-                    // FIX 1: LOVE that you used the newline trick for line returns. The assignment
-                    // wanted you to have extra space after the address. This can be accomplished 2 ways.
-                    // First, you could simply replace "{'\n'}" after "U.S.A." with "{'\n\n'}". Second,
-                    // you could add a bottom margin style to the Text component as the assignment asks.
-                    // OLD CODE: <Text>1 Nucamp Way {'\n'} Seattle, WA 98001 {'\n'}U.S.A {'\n'}</Text>
-                    }
-                    <Text style={{marginBottom: 10}}>1 Nucamp Way {'\n'} Seattle, WA 98001 {'\n'}U.S.A {'\n\n'}</Text>
-                    {
-                    // END FIX 1
-                    }
-                    <Text>Phone: 1-206-555-1234 {'\n'}Email: campsites@nucamp.co</Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card title = "Contact Information"
+                        wrapperStyle={{margin: 20}} >
+                        <Text style={{marginBottom: 10}}>1 Nucamp Way {'\n'} Seattle, WA 98001 {'\n'}U.S.A {'\n\n'}</Text>
+                        <Text>Phone: 1-206-555-1234 {'\n'}Email: campsites@nucamp.co</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
